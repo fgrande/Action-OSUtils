26bb2b1017e1aac9a6beb6343d4ced5dbab9a573
Fabio Grande <fabio.grande@avvale.com>
2025-09-24 15:11:49 +0200

    Added Params

--- entrypoint.ps1
+++ entrypoint.ps1
@@ -1,5 +1,8 @@
-Import-Module -Name ./StringsManager.psm1
-Import-Module -Name ./OSZipper.psm1
-Import-Module -Name ./DocumentationManager.psm1
+Import-Module -Name ./utils/StringsManager.psm1
+Import-Module -Name ./utils/OSZipper.psm1
+Import-Module -Name ./utils/DocumentationManager.psm1
 
-Write-Host "Hello, I should manage : $args!"
+Write-Host "All Args are : $args"
+
+Write-Host "I should manage : $args[0]"
+Write-Host "Additional Param is : $args[1]"
