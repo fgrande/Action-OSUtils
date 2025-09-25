c337b0f8edcad4c231b48f480ec944fb4d6e7b7c
Fabio Grande <fabio.grande@avvale.com>
2025-09-25 09:45:19 +0200

    Changed dir

--- entrypoint.ps1
+++ entrypoint.ps1
@@ -6,20 +6,34 @@ $osAction = $args[0]
 $osCultures = $args[1]
 $osXFProject = $args[2]
 $osSourcesTempDir = $args[3]
+$osNamespacePrefix = $args[4]
+$osAssemblyName = $args[5]
 
 Write-Host "Parameters"
-Write-Host "osAction         : " $osAction
-Write-Host "osCultures       : " $osCultures
-Write-Host "osXFProject      : " $osXFProject
-Write-Host "osSourcesTempDir : " $osSourcesTempDir
+Write-Host "osAction          : " $osAction
+Write-Host "osCultures        : " $osCultures
+Write-Host "osXFProject       : " $osXFProject
+Write-Host "osSourcesTempDir  : " $osSourcesTempDir
+Write-Host "osNamespacePrefix : " $osNamespacePrefix
+Write-Host "osAssemblyName    : " $osAssemblyName
 
 switch ($osAction.ToLower())
 {
     'chkstrings'
     {
         Write-Host "Start Check"
+
         $chkStringsResult = Show-OSStrings -onlyMissing -cultures $osCultures -baseDir $Env:GITHUB_WORKSPACE
-        Write-Host "Result: " $chkStringsResult
+
+        if ($chkStringsResult)
+        {
+            Write-Host "Result: " $chkStringsResult
+        }
+        else
+        {
+            Write-Host "OK ! No missing Strings !"
+        }
+        
         Write-Host "Finish Check"
 
         if ($chkStringsResult)
@@ -35,9 +49,7 @@ switch ($osAction.ToLower())
 
         $sourcesLocation = "${Env:GITHUB_WORKSPACE}/${osSourcesTempDir}"
 
-        format-OSSources $xfprojectLocation $sourcesLocation
-
-        Get-ChildItem $sourcesLocation
+        Format-OSSources -sourceXFProject $xfprojectLocation -destPath $sourcesLocation -wsNamespacePrefix $osNamespacePrefix -wsAssemblyName $osAssemblyName
     }
 }
 
