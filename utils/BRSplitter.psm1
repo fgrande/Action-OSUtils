88bd37ed9dc470c7b7b56bf2d7e0c89b0edb7e04
Fabio Grande <fabio.grande@avvale.com>
2025-11-21 10:41:24 +0100

    Added BRSplitter module

---
+++ utils/BRSplitter.psm1
@@ -0,0 +1,77 @@
+<#
+
+.Synopsis
+Extract Business Rules from xfProject, creating XML files ready to be uploaded
+
+.Description
+Extract Business Rules from xfProject, creating XML files ready to be uploaded
+
+.Parameter sourceXFProject
+The XFProject File definition to get sources from
+
+.Parameter destPath
+Destination Path for manipulated files
+
+.Example
+PS> Split-BRules C:\Works\OneStream\Projects\AWUtils\AWUtils.xfProj c:\temp\awutils
+
+#>
+
+function Split-BRules 
+{
+	Param(
+	  [Parameter(Mandatory=$true)]
+	  [string] $sourceXFProject,
+	  [Parameter(Mandatory=$true)]
+	  [string] $destPath,
+	  [string] $osVersion="8.5.1.17017"
+	)
+
+	# Check if the source file name ends with xfProj
+	if (!$sourceXFProject.EndsWith(".xfProj"))
+	{
+		throw "$sourceXFProject should be an xfProject File !"
+	}
+
+	# First of all the XFProject file must exists, otherwise we exit with an error
+	if (!(Test-Path -Path $sourceXFProject -PathType leaf))
+	{
+		throw "$sourceXFProject doesn't exist - Looks like this is not a OneStream's Project !"
+	}
+
+	# Check if dest Path Exists
+	# If Directory doesn't exists, let's create it 
+	if (!(Test-Path -Path $destPath))
+	{
+		New-Item -Path $destPath -ItemType "Directory" > $null
+	}
+
+	$header = "<?xml version=""1.0"" encoding=""utf-8""?><OneStreamXF version=""$osVersion""><extensibilityRulesRoot>"
+	$footer = "</extensibilityRulesRoot></OneStreamXF>"
+
+	# Get BasePath (the path in which the XFProject file is included)
+	$basePath = Split-Path -Path $sourceXFProject -Parent -Resolve
+
+	# Get all Business Rules Defined
+	foreach ($file in Get-ChildItem -Path $basePath -Recurse *.xml | Where-Object { Select-String "<projectItemType>BusinessRule</projectItemType>" $_ -Quiet })
+	{
+		Write-Output "Found File $($file.FullName)"
+		
+		$brFileName = [io.path]::GetFileName($file.FullName)
+						
+		$mainContent = [xml](Get-Content -Path $file.FullName)
+		
+		$sourceCode = $mainContent.XFProjectFile.content."#cdata-section"[0] + $mainContent.XFProjectFile.content."#cdata-section"[1]
+		$fullSourceCode = "$header$sourceCode$footer"
+
+		$finalFullName = Join-Path -Path $destPath -ChildPath $brFileName
+		Write-Host $finalFullName
+
+		$fullSourceCode | Out-File -FilePath $finalFullName
+	}
+	
+	return $result
+}
+
+
+Export-ModuleMember -Function Split-BRules
